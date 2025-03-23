5302056532f64358b8c1082845b7262ef7fead10
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-03-23 16:22:44 +0200

    Update FinalizeLoadingState.cs

--- Assets/Scripts/Infrastructure/StateMachine/Game/States/FinalizeLoadingState.cs
+++ Assets/Scripts/Infrastructure/StateMachine/Game/States/FinalizeLoadingState.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Data.Models.Static.Core;
+﻿using System;
+using Infrastructure.Data.Models.Static.Core;
+using Infrastructure.LoadingScreen.Core;
 using Infrastructure.Services.Log.Core;
 using Infrastructure.StateMachine.Game.States.Core;
 using Infrastructure.StateMachine.Main.Core;
@@ -11,21 +13,26 @@ namespace Infrastructure.StateMachine.Game.States
         private readonly IStateMachine<IGameState> _stateMachine;
         private readonly IStaticDataModel _staticDataModel;
         private readonly ILogService _logService;
+        private readonly ILoadingScreen _loadingScreen;
 
-        public FinalizeLoadingState(IStateMachine<IGameState> stateMachine, IStaticDataModel staticDataModel, ILogService logService)
+        public FinalizeLoadingState(IStateMachine<IGameState> stateMachine, IStaticDataModel staticDataModel, ILogService logService, ILoadingScreen loadingScreen)
         {
             _stateMachine = stateMachine;
             _staticDataModel = staticDataModel;
             _logService = logService;
+            _loadingScreen = loadingScreen;
         }
 
         public void Enter()
         {
             _logService.Log("Game.FinalizeLoadingState.Enter");
 
+            Progress<float> progress = new Progress<float>(x => _loadingScreen.SetProgress(x));
+
             LoadSceneState.Payload payload = new LoadSceneState.Payload
             {
-                SceneName = _staticDataModel.Config.MenuScene
+                SceneName = _staticDataModel.Config.MenuScene,
+                Progress = progress
             };
 
             _stateMachine.Enter<LoadSceneState, LoadSceneState.Payload>(payload);
