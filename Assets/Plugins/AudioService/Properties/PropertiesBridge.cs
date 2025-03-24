a55d6d23a6940b221bd509dbcb3c5daab6b4060d
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-03-24 19:10:44 +0200

    Update PropertiesBridge.cs

--- Assets/Plugins/AudioService/Properties/PropertiesBridge.cs
+++ Assets/Plugins/AudioService/Properties/PropertiesBridge.cs
@@ -18,14 +18,11 @@ namespace Plugins.AudioService.Properties
             Position = new Property<int, Vector3>(canAccess, id => activePool[id].Audio.Position, (id, value) => activePool[id].Audio.Position = value);
             Rotation = new Property<int, Quaternion>(canAccess, id => activePool[id].Audio.Rotation, (id, value) => activePool[id].Audio.Rotation = value);
             Clip = new Property<int, AudioClip>(canAccess, id => activePool[id].Audio.Clip, (id, value) => activePool[id].Audio.Clip = value);
-            AudioMixerGroup = new Property<int, AudioMixerGroup>(canAccess, id => activePool[id].Audio.AudioMixerGroup,
-                (id, value) => activePool[id].Audio.AudioMixerGroup = value);
+            AudioMixerGroup = new Property<int, AudioMixerGroup>(canAccess, id => activePool[id].Audio.AudioMixerGroup, (id, value) => activePool[id].Audio.AudioMixerGroup = value);
             Mute = new Property<int, bool>(canAccess, id => activePool[id].Audio.Mute, (id, value) => activePool[id].Audio.Mute = value);
             BypassEffects = new Property<int, bool>(canAccess, id => activePool[id].Audio.BypassEffects, (id, value) => activePool[id].Audio.BypassEffects = value);
-            BypassListenerEffects = new Property<int, bool>(canAccess, id => activePool[id].Audio.BypassListenerEffects,
-                (id, value) => activePool[id].Audio.BypassListenerEffects = value);
-            BypassReverbZones = new Property<int, bool>(canAccess, id => activePool[id].Audio.BypassReverbZones,
-                (id, value) => activePool[id].Audio.BypassReverbZones = value);
+            BypassListenerEffects = new Property<int, bool>(canAccess, id => activePool[id].Audio.BypassListenerEffects, (id, value) => activePool[id].Audio.BypassListenerEffects = value);
+            BypassReverbZones = new Property<int, bool>(canAccess, id => activePool[id].Audio.BypassReverbZones, (id, value) => activePool[id].Audio.BypassReverbZones = value);
             Loop = new Property<int, bool>(canAccess, id => activePool[id].Audio.Loop, (id, value) => activePool[id].Audio.Loop = value);
             Priority = new Property<int, int>(canAccess, id => activePool[id].Audio.Priority, (id, value) => activePool[id].Audio.Priority = value);
             Volume = new Property<int, float>(canAccess, id => activePool[id].Audio.Volume, (id, value) => activePool[id].Audio.Volume = value);
@@ -35,8 +32,7 @@ namespace Plugins.AudioService.Properties
             ReverbZoneMix = new Property<int, float>(canAccess, id => activePool[id].Audio.ReverbZoneMix, (id, value) => activePool[id].Audio.ReverbZoneMix = value);
             DopplerLevel = new Property<int, float>(canAccess, id => activePool[id].Audio.DopplerLevel, (id, value) => activePool[id].Audio.DopplerLevel = value);
             Spread = new Property<int, float>(canAccess, id => activePool[id].Audio.Spread, (id, value) => activePool[id].Audio.Spread = value);
-            RolloffMode = new Property<int, AudioRolloffMode>(canAccess, id => activePool[id].Audio.RolloffMode,
-                (id, value) => activePool[id].Audio.RolloffMode = value);
+            RolloffMode = new Property<int, AudioRolloffMode>(canAccess, id => activePool[id].Audio.RolloffMode, (id, value) => activePool[id].Audio.RolloffMode = value);
             MinDistance = new Property<int, float>(canAccess, id => activePool[id].Audio.MinDistance, (id, value) => activePool[id].Audio.MinDistance = value);
             MaxDistance = new Property<int, float>(canAccess, id => activePool[id].Audio.MaxDistance, (id, value) => activePool[id].Audio.MaxDistance = value);
         }
