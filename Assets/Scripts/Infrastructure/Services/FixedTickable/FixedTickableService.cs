5702296ea8250086705005e8150f5414ecafecfc
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-03-29 15:03:41 +0200

    fix: tickable services;

--- Assets/Scripts/Infrastructure/Services/FixedTickable/FixedTickableService.cs
+++ Assets/Scripts/Infrastructure/Services/FixedTickable/FixedTickableService.cs
@@ -1,22 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Infrastructure.Services.FixedTickable.Core;
+using UnityEngine;
 using IFixedTickable = VContainer.Unity.IFixedTickable;
 
 namespace Infrastructure.Services.FixedTickable
 {
     public class FixedTickableService : IFixedTickableService, IFixedTickable
     {
-        private readonly HashSet<Core.IFixedTickable> _fixedTickables = new HashSet<Core.IFixedTickable>();
+        private readonly List<Core.IFixedTickable> _fixedTickables = new List<Core.IFixedTickable>();
+        private readonly List<Core.IFixedTickable> _itemsToAdd = new List<Core.IFixedTickable>();
+        private readonly List<Core.IFixedTickable> _itemsToRemove = new List<Core.IFixedTickable>();
 
-        public void Add(Core.IFixedTickable fixedTickable) => _fixedTickables.Add(fixedTickable);
+        public void Add(Core.IFixedTickable fixedTickable) => _itemsToAdd.Add(fixedTickable);
 
-        public void Remove(Core.IFixedTickable fixedTickable) => _fixedTickables.Remove(fixedTickable);
+        public void Remove(Core.IFixedTickable fixedTickable) => _itemsToRemove.Add(fixedTickable);
 
         public void FixedTick()
         {
             foreach (Core.IFixedTickable fixedTickable in _fixedTickables)
             {
-                fixedTickable.FixedTick();
+                try
+                {
+                    fixedTickable.FixedTick();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+
+            if (_itemsToAdd.Count > 0)
+            {
+                foreach (Core.IFixedTickable itemToAdd in _itemsToAdd)
+                    _fixedTickables.Add(itemToAdd);
+
+                _itemsToAdd.Clear();
+            }
+
+            if (_itemsToRemove.Count > 0)
+            {
+                foreach (Core.IFixedTickable itemToRemove in _itemsToRemove)
+                    _fixedTickables.Remove(itemToRemove);
+
+                _itemsToRemove.Clear();
             }
         }
     }
