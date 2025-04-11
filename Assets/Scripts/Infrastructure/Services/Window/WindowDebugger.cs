424c77b25b80145c8b68436d9bcd4aa9a773c90f
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-04-11 22:17:01 +0300

    feat: added window debugger;

---
+++ Assets/Scripts/Infrastructure/Services/Window/WindowDebugger.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using Infrastructure.Services.Window.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using VContainer;
+
+namespace Infrastructure.Services.Window
+{
+    public class WindowDebugger : MonoBehaviour
+    {
+        private IWindowService _windowService;
+
+        [Inject]
+        public void Construct(IWindowService windowService) => _windowService = windowService;
+
+        [Button]
+        private void CreateWindow(WindowID id) => _windowService.CreateWindow(id).ContinueWith(window => window.Show()).Forget();
+
+        [Button]
+        private void DestroyTopWindow()
+        {
+            IWindow window = _windowService.GetTopWindow();
+
+            if (window != null)
+                Destroy(window.RootRectTransform.gameObject);
+        }
+    }
+}
