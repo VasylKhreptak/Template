efabe706e1a1dcf80ba314bcf4c6a3ccc4f80043
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-04-13 21:23:15 +0300

    chore: added test scene;

--- Assets/Scripts/AddressablesTest.cs
+++ Assets/Scripts/AddressablesTest.cs
@@ -1,35 +1,46 @@
 ﻿using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
-using Infrastructure.Services.Asset.Core;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using VContainer;
 
 public class AddressablesTest : MonoBehaviour
 {
     [SerializeField] private AssetReferenceGameObject _assetReference;
 
-    private IAssetService _assetService;
+    // private IAssetService _assetService;
+    //
+    // [Inject]
+    // public void Construct(IAssetService assetService)
+    // {
+    //     _assetService = assetService;
+    // }
 
-    [Inject]
-    public void Construct(IAssetService assetService)
-    {
-        _assetService = assetService;
-    }
-
-    private List<GameObject> _instances = new List<GameObject>();
+    private readonly List<GameObject> _instances = new List<GameObject>();
 
     private void OnGUI()
     {
         if (GUILayout.Button("Instantiate"))
         {
-            _assetService.InstantiateAsync(_assetReference).ContinueWith(x => _instances.Add(x)).Forget();
+            // if (_assetService == null)
+            Addressables.InstantiateAsync(_assetReference).ToUniTask().ContinueWith(x => _instances.Add(x)).Forget();
+            // else
+            // _assetService.InstantiateAsync(_assetReference).ContinueWith(x => _instances.Add(x)).Forget();
         }
 
         if (GUILayout.Button("Destroy"))
         {
+            // if (_assetService == null)
+            // {
+
             foreach (GameObject instance in _instances)
-                Destroy(instance);
+                Addressables.ReleaseInstance(instance);
+
+            // }
+            // else
+            // {
+            // foreach (GameObject instance in _instances)
+            // Destroy(instance);
+            // }
 
             _instances.Clear();
         }
