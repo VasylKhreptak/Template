dcd365fd2fef515295cc82cf78915d9f9b571b0b
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-04-14 20:11:26 +0300

    Revert "chore: added test scene;"
    This reverts commit efabe706e1a1dcf80ba314bcf4c6a3ccc4f80043.

--- Assets/Scripts/AddressablesTest.cs
+++ Assets/Scripts/AddressablesTest.cs
@@ -1,46 +1,35 @@
 ﻿using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using Infrastructure.Services.Asset.Core;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using VContainer;
 
 public class AddressablesTest : MonoBehaviour
 {
     [SerializeField] private AssetReferenceGameObject _assetReference;
 
-    // private IAssetService _assetService;
-    //
-    // [Inject]
-    // public void Construct(IAssetService assetService)
-    // {
-    //     _assetService = assetService;
-    // }
+    private IAssetService _assetService;
 
-    private readonly List<GameObject> _instances = new List<GameObject>();
+    [Inject]
+    public void Construct(IAssetService assetService)
+    {
+        _assetService = assetService;
+    }
+
+    private List<GameObject> _instances = new List<GameObject>();
 
     private void OnGUI()
     {
         if (GUILayout.Button("Instantiate"))
         {
-            // if (_assetService == null)
-            Addressables.InstantiateAsync(_assetReference).ToUniTask().ContinueWith(x => _instances.Add(x)).Forget();
-            // else
-            // _assetService.InstantiateAsync(_assetReference).ContinueWith(x => _instances.Add(x)).Forget();
+            _assetService.InstantiateAsync(_assetReference).ContinueWith(x => _instances.Add(x)).Forget();
         }
 
         if (GUILayout.Button("Destroy"))
         {
-            // if (_assetService == null)
-            // {
-
             foreach (GameObject instance in _instances)
-                Addressables.ReleaseInstance(instance);
-
-            // }
-            // else
-            // {
-            // foreach (GameObject instance in _instances)
-            // Destroy(instance);
-            // }
+                Destroy(instance);
 
             _instances.Clear();
         }
